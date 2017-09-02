19a01bb34243c53b1c2e41039308c26ac9f689c5
Entrivax <entrivax@gmail.com>
2017-09-02 17:15:51 +0200

    add IPacket interface

---
+++ NetCraft.Core/Packets/IPacket.cs
@@ -0,0 +1,21 @@
+﻿using NetCraft.Core.Network;
+
+namespace NetCraft.Core.Packets
+{
+    interface IPacket
+    {
+        /// <summary>
+        /// This method is used to read data of the packet
+        /// </summary>
+        /// <param name="stream"></param>
+        void ReadPacketData(JavaDataStream stream);
+
+        /// <summary>
+        /// This method is used to write data of the packet
+        /// </summary>
+        /// <param name="stream"></param>
+        void WritePacketData(JavaDataStream stream);
+
+        int Size { get; }
+    }
+}
