8c5ad7bca5859c0adb2b4b70ce140895fa7c8eab
Entrivax <entrivax@gmail.com>
2017-09-02 23:57:23 +0200

    add packets from 13 to 17

---
+++ NetCraft.Core/Packets/Packet16BlockItemSwitch.cs
@@ -0,0 +1,21 @@
+﻿using NetCraft.Core.Network;
+
+namespace NetCraft.Core.Packets
+{
+    public class Packet16BlockItemSwitch : IPacket
+    {
+        public int Size => 2;
+
+        public int Id { get; set; }
+
+        public void ReadPacketData(JavaDataStream stream)
+        {
+            Id = stream.ReadInt16();
+        }
+
+        public void WritePacketData(JavaDataStream stream)
+        {
+            stream.WriteInt16((short)Id);
+        }
+    }
+}
