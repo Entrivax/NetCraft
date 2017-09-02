75a4efb588f8014d06feff4c873029cd5bfce58a
Entrivax <entrivax@gmail.com>
2017-09-02 18:15:29 +0200

    Add packets from 10 to 12

---
+++ NetCraft.Core/Packets/Packet10Flying.cs
@@ -0,0 +1,21 @@
+﻿using NetCraft.Core.Network;
+
+namespace NetCraft.Core.Packets
+{
+    public class Packet10Flying : IPacket
+    {
+        public int Size => 1;
+
+        public bool OnGround { get; set; }
+
+        public void ReadPacketData(JavaDataStream stream)
+        {
+            OnGround = stream.ReadByte() != 0;
+        }
+
+        public void WritePacketData(JavaDataStream stream)
+        {
+            stream.WriteByte(OnGround ? (byte)1 : (byte)0);
+        }
+    }
+}
---
+++ NetCraft.Core/Packets/Packet11PlayerPosition.cs
@@ -0,0 +1,34 @@
+﻿using NetCraft.Core.Network;
+
+namespace NetCraft.Core.Packets
+{
+    public class Packet11PlayerPosition : IPacket
+    {
+        public int Size => 33;
+
+        public double XPosition { get; set; }
+        public double YPosition { get; set; }
+        public double ZPosition { get; set; }
+        public double Stance { get; set; }
+        public bool OnGround { get; set; }
+        public bool Moving { get; set; } = true;
+
+        public void ReadPacketData(JavaDataStream stream)
+        {
+            XPosition = stream.ReadDouble();
+            YPosition = stream.ReadDouble();
+            Stance = stream.ReadDouble();
+            ZPosition = stream.ReadDouble();
+            OnGround = stream.ReadByte() != 0;
+        }
+
+        public void WritePacketData(JavaDataStream stream)
+        {
+            stream.WriteDouble(XPosition);
+            stream.WriteDouble(YPosition);
+            stream.WriteDouble(Stance);
+            stream.WriteDouble(ZPosition);
+            stream.WriteByte(OnGround ? (byte)1 : (byte)0);
+        }
+    }
+}
