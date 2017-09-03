efbc84a2d0bcc7883054bf0848109397605acb5d
Clement.Lebouis <clement.lebouis@etu.univ-orleans.fr>
2017-09-03 19:04:22 +0200

    Ajout WatchableObject

---
+++ NetCraft.Core/Watcher/WatchableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class WatchableObject
+{
+    public readonly int objectType { get; }
+    public readonly int dataValueId { get; }
+    public Object watchedObject { get; set; }
+    public bool isWatching { set; }
+
+    public WatchableObject(int objectType, int dataValueId, Object watchedObject)
+	{
+        this.objectType = objectType;
+        this.dataValueId = dataValueId;
+        this.watchedObject = watchedObject;
+        this.isWatching = true;
+	}
+}
