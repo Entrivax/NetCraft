162f216ea88bdcb03569428a6d468f5245bb99a1
Eclegv <eclegv11@gmail.com>
2017-09-03 03:33:31 +0200

    Ajout Packet255

---
+++ NetCraft.Core/Packets/Packet255KickDisconnect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetCraft.Core.Network;
+using System.IO;
+
+namespace NetCraft.Core.Packets
+{
+    class Packet255KickDisconnect : IPacket
+    {
+        public String reason;
+
+        public Packet255KickDisconnect()
+        {
+        }
+
+        public Packet255KickDisconnect(String s)
+        {
+            reason = s;
+        }
+
+        public int Size => reason.Length;
+
+        public void ReadPacketData(JavaDataStream stream)
+        {
+            if (stream.ReadString().Length > 100)
+                throw new IOException();
+            reason = stream.ReadString();
+        }
+
+        public void WritePacketData(JavaDataStream stream)
+        {
+            stream.WriteString(reason);
+        }
+    }
+}
