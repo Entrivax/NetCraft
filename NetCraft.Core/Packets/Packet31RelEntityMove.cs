3fe29b1e531e097f3828f013eafb19f33e47ee5e
Entrivax <entrivax@hotmail.fr>
2017-09-10 11:16:47 +0200

    Add packets 3 and from 30 to 39

---
+++ NetCraft.Core/Packets/Packet31RelEntityMove.cs
@@ -0,0 +1,30 @@
+﻿using NetCraft.Core.Network;
+
+namespace NetCraft.Core.Packets
+{
+    public class Packet31RelEntityMove : IPacket
+    {
+        public int Size => 7;
+
+        public int EntityId { get; set; }
+        public byte XPosition { get; set; }
+        public byte YPosition { get; set; }
+        public byte ZPosition { get; set; }
+
+        public void ReadPacketData(JavaDataStream stream)
+        {
+            EntityId = stream.ReadInt32();
+            XPosition = (byte)stream.ReadByte();
+            YPosition = (byte)stream.ReadByte();
+            ZPosition = (byte)stream.ReadByte();
+        }
+
+        public void WritePacketData(JavaDataStream stream)
+        {
+            stream.WriteInt32(EntityId);
+            stream.WriteByte(XPosition);
+            stream.WriteByte(YPosition);
+            stream.WriteByte(ZPosition);
+        }
+    }
+}
---
+++ NetCraft.Core/Packets/Packet3Chat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetCraft.Core.Network;
+using System.IO;
+
+namespace NetCraft.Core.Packets
+{
+    public class Packet3Chat : IPacket
+    {
+        public int Size => throw new NotImplementedException();
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value.Length > 119 ? value.Substring(0, 119) : value; }
+        }
+
+        public void ReadPacketData(JavaDataStream stream)
+        {
+            var message = stream.ReadString();
+            if (message.Length > 119)
+                throw new IOException();
+            Message = message;
+        }
+
+        public void WritePacketData(JavaDataStream stream)
+        {
+            stream.WriteString(Message);
+        }
+    }
+}
