8075d140c3346b13d457546f2b997ff54de878ae
Entrivax <entrivax@gmail.com>
2017-09-17 18:37:01 +0200

    Some tests to try to communicate with a real client

--- NetCraft.Core/Packets/Packet101CloseWindow.cs
+++ NetCraft.Core/Packets/Packet101CloseWindow.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using NetCraft.Core.Network;
+﻿using NetCraft.Core.Network;
 
 namespace NetCraft.Core.Packets
 {
     public class Packet101CloseWindow : IPacket
     {
-
-        public int windowId;
-
-        Packet101CloseWindow()
-        {
-
-        }
-
-        public Packet101CloseWindow(int i)
-        {
-            windowId = i;
-        }
+        public int WindowId { get; set; }
 
         public int Size => 1;
 
         public void ReadPacketData(JavaDataStream stream)
         {
-            windowId = stream.ReadByte();
+            WindowId = stream.ReadByte();
         }
 
         public void WritePacketData(JavaDataStream stream)
         {
-            stream.WriteByte((byte)windowId);
+            stream.WriteByte((byte)WindowId);
         }
     }
 }
--- NetCraft/Program.cs
+++ NetCraft/Program.cs
@@ -24,8 +24,14 @@ namespace NetCraft
             pm.RegisterPacketHandler<Packet13PlayerLookMove>(13, (p, j, pa) => LogPacket(pa));
             pm.RegisterPacketHandler<Packet14BlockDig>(14, (p, j, pa) => LogPacket(pa));
             pm.RegisterPacketHandler<Packet5PlayerInventory>(5, (p, j, pa) => LogPacket(pa));
+            pm.RegisterPacketHandler<Packet101CloseWindow>(101, handleCloseWindow);
 
             pm.RegisterPacketId<Packet1Login>(1);
+            pm.RegisterPacketId<Packet2Handshake>(2);
+            pm.RegisterPacketId<Packet4UpdateTime>(4);
+            pm.RegisterPacketId<Packet6SpawnPosition>(6);
+            pm.RegisterPacketId<Packet50PreChunk>(50);
+            pm.RegisterPacketId<Packet101CloseWindow>(101);
             while (true)
                 pm.HandlePackets(js);
         }
@@ -35,10 +41,20 @@ namespace NetCraft
             Console.WriteLine(JsonConvert.SerializeObject(packet));
         }
 
+        public static void handleCloseWindow(PacketManager packetManager, JavaDataStream stream, Packet101CloseWindow packet)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(packet));
+            packetManager.SendPacket(stream, new Packet101CloseWindow { WindowId = packet.WindowId });
+        }
+
         public static void handleHandshake(PacketManager packetManager, JavaDataStream stream, Packet2Handshake packet)
         {
             Console.WriteLine(JsonConvert.SerializeObject(packet));
+            packetManager.SendPacket(stream, new Packet2Handshake { Username = "-" });
             packetManager.SendPacket(stream, new Packet1Login { Username = "-", Dimension = 0, MapSeed = 43567, ProtocolVersion = 1 });
+            packetManager.SendPacket(stream, new Packet6SpawnPosition { XPosition = 0, YPosition = 20, ZPosition = 0 });
+            packetManager.SendPacket(stream, new Packet4UpdateTime { Time = 12000 });
+            packetManager.SendPacket(stream, new Packet50PreChunk { XPosition = 0, YPosition = 0, Mode = true });
         }
     }
 }
